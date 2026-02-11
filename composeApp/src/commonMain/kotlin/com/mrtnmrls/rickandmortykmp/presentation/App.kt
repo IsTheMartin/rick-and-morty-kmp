c497d6e9ba361782878447db772bf71b6b3c2901
Martin Morales <themartin93@gmail.com>
2026-02-11 12:52:31 -0600

    Linter format

--- composeApp/src/commonMain/kotlin/com/mrtnmrls/rickandmortykmp/presentation/App.kt
+++ composeApp/src/commonMain/kotlin/com/mrtnmrls/rickandmortykmp/presentation/App.kt
@@ -3,7 +3,6 @@ package com.mrtnmrls.rickandmortykmp.presentation
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.CompositionLocalProvider
-import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
